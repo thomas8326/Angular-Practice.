027d7d178f00deac04e9ba1798200d2b5146507a
thomas8326 <tt7919@gmail.com>
2019-12-04 17:41:38 +0800

    3 files modified

--- src/app/infinite-scroll-testing/infinite-scroll-testing.component.ts
+++ src/app/infinite-scroll-testing/infinite-scroll-testing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-infinite-scroll-testing',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfiniteScrollTestingComponent implements OnInit {
 
+  @ViewChild('sensor', {static: true}) sensor: ElementRef;
+  list: string[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.renderList();
+    this.intersectionObserable();
+  }
+
+  renderList() {
+    for(let i = 0; i < 100; i++) {
+      this.list.push(`item${i}`);
+    }
+  }
+
+  intersectionObserable() {
+    new IntersectionObserver(([entry])=> console.log(entry), {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5
+    }).observe(this.sensor.nativeElement);
   }
 
 }
