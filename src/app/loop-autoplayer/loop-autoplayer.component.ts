f1ea83e9e1206addef356ef7cea38170ada49c15
thomas8326 <tt7919@gmail.com>
2019-12-17 19:49:48 +0800

    2 files modified

--- src/app/loop-autoplayer/loop-autoplayer.component.ts
+++ src/app/loop-autoplayer/loop-autoplayer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Renender2 } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-loop-autoplayer',
@@ -8,16 +8,23 @@ import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Renender2 } fr
 export class LoopAutoplayerComponent implements AfterViewInit {
 
   @ViewChild('autoplayer',  {static: true}) autoplayer: ElementRef<any>;
+  @ViewChild('div1',  {static: true}) div1: ElementRef<any>;
+  @ViewChild('div2',  {static: true}) div2: ElementRef<any>;
 
-  constructor(private renender: Renender2) { }
+  constructor(private renender: Renderer2) { }
 
   ngAfterViewInit() {
     console.log(this.autoplayer.nativeElement.getBoundingClientRect());
   }
 
   goLeft() {
-    this.renender.setStyle(this.autoplayer.nativeElement, 'left', '10px');
-    console.log(this.autoplayer.nativeElement.getBoundingClientRect());
+    this.renender.setStyle(this.div1.nativeElement, 'display', 'none');
+    this.renender.setStyle(this.div2.nativeElement, 'display', 'flex');
+  }
+
+  goRight() {
+    this.renender.setStyle(this.div1.nativeElement, 'display', 'flex');
+    this.renender.setStyle(this.div2.nativeElement, 'display', 'none');
   }
 
 }
