13725b82afbf0f62a4ebc7c822f905fe30418597
thomas8326 <tt7919@gmail.com>
2019-12-19 15:55:08 +0800

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -11,7 +12,7 @@ import { LoopAutoplayerComponent } from './loop-autoplayer/loop-autoplayer.compo
 import { SlideshowComponent } from './slide-show/slide-show.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule ],
   declarations: [ AppComponent, HelloComponent, ChatCardComponent, InfiniteScrollTestingComponent, IntersectionIndicatorComponent, LoopAutoplayerComponent, SlideshowComponent ],
   bootstrap:    [ AppComponent ]
 })
