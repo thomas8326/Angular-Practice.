8ca26a9ce93723b35fca5d671270f56f0aba2eae
thomas8326 <tt7919@gmail.com>
2019-12-19 14:03:03 +0800

    add slide show

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { ChatCardComponent } from './chat-card/chat-card.component';
 import { InfiniteScrollTestingComponent } from './infinite-scroll-testing/infinite-scroll-testing.component';
 import { IntersectionIndicatorComponent } from './infinite-scroll-testing/intersection-indicator/intersection-indicator.component';
 import { LoopAutoplayerComponent } from './loop-autoplayer/loop-autoplayer.component';
+import { SlideshowComponent } from './slide-show/slide-show.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ChatCardComponent, InfiniteScrollTestingComponent, IntersectionIndicatorComponent, LoopAutoplayerComponent ],
+  declarations: [ AppComponent, HelloComponent, ChatCardComponent, InfiniteScrollTestingComponent, IntersectionIndicatorComponent, LoopAutoplayerComponent, SlideshowComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
