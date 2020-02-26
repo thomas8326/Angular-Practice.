475ab1a05644dad0f6cb2032aee9f4e43dfbb84b
thomas8326 <tt7919@gmail.com>
2020-02-26 22:31:51 +0800

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,11 @@ import { IntersectionIndicatorComponent } from './infinite-scroll-testing/inters
 import { LoopAutoplayerComponent } from './loop-autoplayer/loop-autoplayer.component';
 import { SlideshowComponent } from './slide-show/slide-show.component';
 import { PracticeI18nComponent } from './practice-i18n/practice-i18n.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule ],
-  declarations: [ AppComponent, HelloComponent, ChatCardComponent, InfiniteScrollTestingComponent, IntersectionIndicatorComponent, LoopAutoplayerComponent, SlideshowComponent, PracticeI18nComponent ],
+  declarations: [ AppComponent, HelloComponent, ChatCardComponent, InfiniteScrollTestingComponent, IntersectionIndicatorComponent, LoopAutoplayerComponent, SlideshowComponent, PracticeI18nComponent, ResumeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
